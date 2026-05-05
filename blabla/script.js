4ff807ed0d0fa52f9b8d797f20036dce6e3ad92f
Oanekrif <130631445+Oanekrif@users.noreply.github.com>
2026-05-05 20:12:24 +0100

    add

--- blabla/script.js
+++
@@ -1,6 +0,0 @@
-if (localStorage.length === 1)
-    document.querySelector(".name").value = localStorage.name;
-
-document.querySelector(".name").onblur = function () {
-    localStorage.setItem("name", this.value);
-}
